5901d403ee8ef1797079392f0d641cee85fb9be3
younsiamed <younsiamed@gmail.com>
2024-07-07 20:07:55 +0300

    Lesson-5-6

---
+++ Печать контейнера/Печать контейнера.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <set>
+#include <vector>
+#include <list>
+
+template <typename Container>
+void print_container(const Container& container) {
+    for (auto it = container.begin(); it != container.end(); ++it) {
+        if (it != container.begin()) {
+            std::cout << ", ";
+        }
+        std::cout << *it;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    SetConsoleOutputCP(65001);
+
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four, one, three, two
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+
+    return 0;
+}
---
+++ Уникальный вектор/Уникальный вектор.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+std::vector<int> removeDuplicates(std::vector<int>& vec) {
+    std::sort(vec.begin(), vec.end());
+
+    auto last = std::unique(vec.begin(), vec.end());
+
+    vec.erase(last, vec.end());
+
+    return vec;
+}
+
+int main() {
+    SetConsoleOutputCP(65001);
+    std::cout << "[IN]: ";
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::istringstream stream(input);
+    std::vector<int> vec;
+    int number;
+
+    while (stream >> number) {
+        vec.push_back(number);
+    }
+
+    vec = removeDuplicates(vec);
+
+    std::cout << "[OUT]: ";
+    for (const int& num : vec) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
